aff629168e928489d87e950da09b0808a73e2d69
sojeongLee19 <55444000+sojeongLee19@users.noreply.github.com>
2019-11-26 15:25:25 +0900

    1

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+    struct student { 
+     int   ID;
+    char   name[10];
+     double grade;
+	  }; 
+int main (void) { 
+struct student s1 = {24, "sojeong", 4.3};
+}
+
